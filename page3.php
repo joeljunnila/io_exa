62d1a649498da432e1889ca8af3067a09b8366b0
joeljunnila <joel.junnila@gmail.com>
2020-01-26 22:14:40 +0200

    ex2 ready

---
+++ page3.php
@@ -0,0 +1,6 @@
+<h1>Page3</h1>
+<ul>
+<li> <a href="#" onclick="loadDoc('page1.html')">Page1</a> </li>
+<li> <a href="#" onclick="loadDoc('page2.html')">Page2</a> </li>
+<li> <a href="#" onclick="loadDoc('page3.php')">Page3</a> </li>
+</ul>
